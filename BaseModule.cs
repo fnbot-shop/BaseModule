ba15f3d856275280a8ccf0723b28fedb3c2de1ef
Aleks Margarian <aleks.margarian1@gmail.com>
2020-02-18 00:00:00 -0800

    Initial commit

---
+++ BaseModule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using fnbot.shop.Backend;
+using fnbot.shop.Backend.Configuration;
+using fnbot.shop.Backend.ItemTypes;
+
+namespace BaseModule
+{
+    public sealed class BaseModule : IModule
+    {
+        // Usually in seconds
+        public int RefreshTime => 3;
+        public IConfig Config => config;
+
+        Config config = new Config();
+        Random random = new Random();
+
+        // Respect the force argument and return a non-null item if true
+        public async Task<IItem> Post(bool force)
+        {
+            if (force || random.NextDouble() > .8f)
+            {
+                Console.WriteLine("PRINTING " + config.PostMessage.Value);
+                return new ItemText(config.PostMessage.Value);
+            }
+            return null;
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
---
+++ Config.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using fnbot.shop.Backend.Configuration;
+
+namespace BaseModule
+{
+    sealed class Config : IConfig
+    {
+        // You are to handle everything when working with previous versions, invalid data, etc.
+        public void LoadConfig(Stream stream)
+        {
+            using var reader = new BinaryReader(stream);
+            PostMessage.Value = reader.ReadString();
+        }
+
+        // The file is truncated for you, write as it's a new file
+        public void SaveConfig(Stream stream)
+        {
+            using var writer = new BinaryWriter(stream);
+            writer.Write(PostMessage.Value);
+        }
+
+        public readonly ConfigProperty<string> PostMessage = new ConfigProperty<string>("This is a test message.", "Post Message", true, true);
+    }
+}
